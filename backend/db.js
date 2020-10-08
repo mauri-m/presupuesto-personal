3e6c26903d0d2e6fad221a57b101207df69276d5
Mauricio Melgarejo <maury__25@hotmail.com>
2020-10-08 19:08:27 -0300

    feat: added initial database config

---
+++ backend/db.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+const ingresosModel = require('./models/ingresos');
+const egresosModel = require('./models/egresos');
+const usersModel = require('./models/users');
+
+//
+const sequelize = new Sequelize('presupuesto', 'root', '', {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false
+});
+
+
+const Ingreso = ingresosModel(sequelize, Sequelize);
+const Egreso = egresosModel(sequelize, Sequelize);
+const User = usersModel(sequelize, Sequelize);
+
+sequelize.sync({
+        force: false
+    })
+    .then(() => console.log('Success'))
+    .catch((error) => console.log(error))
+
+
+module.exports = {
+    Ingreso,
+    Egreso,
+    User
+}
